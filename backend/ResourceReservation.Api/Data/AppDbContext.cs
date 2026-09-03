a4af7a27d5b34a0f338fcae1dbe07f9cd1bc9273
Andershgras <andershgras@gmail.com>
2026-09-03 21:53:40 +0200

    Add application database context

---
+++ backend/ResourceReservation.Api/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using ResourceReservation.Api.Models;
+
+namespace ResourceReservation.Api.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Resource> Resources { get; set; }
+}
