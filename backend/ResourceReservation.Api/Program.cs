66e18b6a6275abfeb1927a9c4d1cc5320a6abd67
Andershgras <andershgras@gmail.com>
2026-09-03 22:03:17 +0200

    Configure SQL Server database context

--- backend/ResourceReservation.Api/Program.cs
+++ backend/ResourceReservation.Api/Program.cs
@@ -1,6 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using ResourceReservation.Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
